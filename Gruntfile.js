2f8753f202ac0f359df19b7262aa4b481a5d69eb
stanik123 <stanikdoubravsky@gmail.com>
2023-07-20 10:17:56 +0200

    Now almost fully ported
    Now fully resolved file naming, completed grunt-bake automation, reunited new site name Chemical gastronomy or shortly Chemi Gastro

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,28 +3,110 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		bake: {
-			files: {
-				//"/src/cz/index.html": "/docs/cz/domu.html",
-				//"/src/cz/news.html": "/docs/cz/novinky.html"
-				expand: true,
-				cwd: "src/",
-				flatten: false,
-				src: ["**/*.html", "!*includes*", "!*templates*"],
-				dest: "docs/",
-				ext: ".html"
+			build: {
+				files: [{
+					//"docs/cz/domu.html": "src/cz/index.html",
+					//"docs/cz/novinky.html": "src/cz/news.html"
+					expand: true,
+					cwd: "src/",
+					flatten: false,
+					src: ["**/*.html", "!*includes*", "!*templates*"],
+					dest: "docs/",
+					ext: ".html",
+					rename: function (dest, src) {
+						switch (true) {
+							case src.includes("cz/"):
+								//grunt.log.ok(src);
+								file_names = grunt.file.readJSON('src/cz/file_names.json');
+								for (var key in file_names) {
+									src = src.replace(key, file_names[key]);
+									//grunt.log.ok(key + " -> " + file_names[key]);
+								}
+								//grunt.log.ok(src);
+								break;
+							case src.includes("de/"):
+								file_names = grunt.file.readJSON('src/de/file_names.json');
+								for (var key in file_names) {
+									src = src.replace(key, file_names[key]);
+								}
+								break;
+							case src.includes("en/"):
+								file_names = grunt.file.readJSON('src/en/file_names.json');
+								for (var key in file_names) {
+									src = src.replace(key, file_names[key]);
+								}
+								break;
+							case src.includes("sk/"):
+								file_names = grunt.file.readJSON('src/sk/file_names.json');
+								for (var key in file_names) {
+									src = src.replace(key, file_names[key]);
+								}
+								break;
+						}
+						return dest + src;
+					}
+				}]
 			}
 		},
+		testDest: {
+			build: {
+				files: [{
+					//"docs/cz/domu.html": "src/cz/index.html",
+					//"docs/cz/novinky.html": "src/cz/news.html"
+					expand: true,
+					cwd: "src/",
+					flatten: false,
+					src: ["**/*.html", "!*/includes/*", "!templates/*"],
+					dest: "docs/",
+					ext: ".html",
+					rename: function (dest, src) {
+						switch (true) {
+							case src.includes("cz/"):
+								//grunt.log.ok(src);
+								file_names = grunt.file.readJSON('src/cz/file_names.json');
+								for (var key in file_names) {
+									src = src.replace(key, file_names[key]);
+									//grunt.log.ok(key + " -> " + file_names[key]);
+								}
+								//grunt.log.ok(src);
+								break;
+							case src.includes("de/"):
+								file_names = grunt.file.readJSON('src/de/file_names.json');
+								for (var key in file_names) {
+									src = src.replace(key, file_names[key]);
+								}
+								break;
+							case src.includes("en/"):
+								file_names = grunt.file.readJSON('src/en/file_names.json');
+								for (var key in file_names) {
+									src = src.replace(key, file_names[key]);
+								}
+								break;
+							case src.includes("sk/"):
+								file_names = grunt.file.readJSON('src/sk/file_names.json');
+								for (var key in file_names) {
+									src = src.replace(key, file_names[key]);
+								}
+								break;
+						}
+						return dest + src;
+					}
+				}]
+			}
+        },
 		sass: {
 			options: {
 				embedSourceMap: true
 			},
-			files: {
-				expand: true,
-				cwd: "src/",
-				flatten: false,
-				src: ["**/*.scss"],
-				dest: "docs/",
-				ext: ".css"
+			build: {
+				files: [{
+					expand: true,
+					cwd: "src/",
+					flatten: false,
+					src: ["**/*.scss"],
+					dest: "docs/",
+					ext: ".css"
+				}]
 			}
 		},
 		watch: {
@@ -48,4 +130,17 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['watch']);
+
+	grunt.registerMultiTask('testDest', function() {
+			this.files.forEach( function( file ) {
+					grunt.log.ok( "File \"" + file.dest + "\"" );
+				}
+			);
+			//mapping = grunt.file.readJSON('src/cz/file_names.json');
+			//grunt.log.ok(mapping.mapping[0]);
+			//for (var key in mapping) {
+			//	grunt.log.ok(key + " -> " + mapping[key]);
+			//}
+        }
+    );
 };
--- sassy.bat
+++
@@ -1,2 +0,0 @@
-cd "C:\users\Stanik\Programing\Web\stanik123.github.io"
-sass --watch src/styles:docs/styles --embed-source-map
